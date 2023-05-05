d4c2c222a09ca4ad22541b82a87022e742bb0f5f
Stella Mazzillo <stellagress5@gmail.com>
2023-05-05 18:13:59 -0400

    adds ol and ul

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
 
-console.log("TESTING")
+
 
 
 
 fetch('http://localhost:3000/planets')
     .then(response=> response.json())
     .then(planets =>{
+        const firstList = document.querySelector("ol#mainPlanetList")
         const allPlanets = document.querySelector("ul#planetList")
         planets.forEach(planet => {
+           
+            //Ordered List of planets: 
+            const mainPlanetList = document.createElement("li")
+            const name = document.createElement("h3")
+            name.textContent = planet.name
+            mainPlanetList.appendChild(name)
+
+            firstList.appendChild(mainPlanetList)
+            
+            //Unordered list with respective dersired information
+            
+
 
             const planetName = document.createElement("h3")
             planetName.textContent = planet.name
 
-            const planetSize = document.createElement("h3")
-            planetSize.textContent = planet.size
+            const planetSize = document.createElement("h4")
+            planetSize.textContent = `Size: ${planet.size}`
 
+            const planetColor = document.createElement("h4")
+            planetColor.textContent = `Predominant color: ${planet.predominantColor}`
+
+            const planetTemp = document.createElement("h4")
+            planetTemp.textContent = `Average temperature: ${planet.averageTemperature}`
+
+            
             const planetList = document.createElement("li")
-            planetList.append(planetName, planetSize)
-            //planetList.appendChild(planetSize)
-
+            planetList.append(planetName, planetSize, planetColor, planetTemp)
+           
             allPlanets.appendChild(planetList)
 
         })
     })
+
+ 
 
 
 
