71955271251ff329604ded0e7d779e8723ab987c
Stella Mazzillo <stellagress5@gmail.com>
2023-05-05 17:14:31 -0400

    adds size and name tags to localhost:3000

--- main.js
+++ main.js
@@ -4,8 +4,25 @@ console.log("TESTING")
 
 
 fetch('http://localhost:3000/planets')
-.then(response=> response.json())
-.then(data => console.log(data))
+    .then(response=> response.json())
+    .then(planets =>{
+        const allPlanets = document.querySelector("ul#planetList")
+        planets.forEach(planet => {
+
+            const planetName = document.createElement("h3")
+            planetName.textContent = planet.name
+
+            const planetSize = document.createElement("h3")
+            planetSize.textContent = planet.size
+
+            const planetList = document.createElement("li")
+            planetList.append(planetName, planetSize)
+            //planetList.appendChild(planetSize)
+
+            allPlanets.appendChild(planetList)
+
+        })
+    })
 
 
 
