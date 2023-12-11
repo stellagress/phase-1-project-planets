1a70218054ca4f1e9a9b6407cd415c413ed68e44
Stella Mazzillo <stellagress5@gmail.com>
2023-12-10 22:10:57 -0500

    changes server.js and moved css and main into public folder

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
+// const jsonServer = require('json-server')
+
+// const server = jsonServer.create()
+
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+ 
+// server.use(middlewares)
+// server.use('/api', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
+
+
+
+const express = require('express');
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Rest of your server code
 const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+app.use(middlewares);
+app.use('/api', router);
 
-
-
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // Serve static files from the 'public' directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Route for serving index.html
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
